31474a2c42066ab6fdc9d6f417fd330d9c9a773b
mortezagolzan <mo.golzan@yahoo.com>
2018-09-05 17:57:23 +0430

    i uploaded the project

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+$("ul").on("click","li",function(){
+  $(this).toggleClass("completed")
+})
+
+$("ul").on("click","span",function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  })
+  event.stopPropagation();
+})
+
+
+$("input[type='text']").on("keypress",function(event){
+  if (event.which===13) {
+    var toDoList = $(this).val();
+    $(this).val("")
+    $("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + toDoList +"</li>")
+
+  }})
+
+  $(".fa-plus").on("click",function(){
+    $("input[type='text']").fadeToggle()
+  })
